7a581cbdf4531f202a6638d194c94f8ad4877e81
Julius Roob <julius@juliusroob.de>
2016-07-19 22:49:29 +0200

    refractoring portal

--- src/main/java/com/z0ttel/z0craft/blocks/BlockZ0Portal.java
+++ src/main/java/com/z0ttel/z0craft/blocks/BlockZ0Portal.java
@@ -1,6 +1,9 @@
 package com.z0ttel.z0craft.blocks;
 
 import java.util.List;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import javax.annotation.Nullable;
 
 import net.minecraft.block.material.Material;
@@ -8,6 +11,9 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockBreakable;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.block.material.MapColor;
+
+import net.minecraft.crash.CrashReport;
+import net.minecraft.crash.ICrashReportDetail;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -20,11 +26,15 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldServer;
 import net.minecraft.world.Teleporter;
 
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.ReportedException;
 
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.fml.relauncher.Side;
+
+import net.minecraftforge.fml.common.CertificateHelper;
 
 import com.z0ttel.z0craft.Z0Craft;
 import com.z0ttel.z0craft.dimension.Z0Teleporter;
@@ -61,6 +71,24 @@ public class BlockZ0Portal extends BlockBreakable {
 	@Override
 	public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn)
 	{
+		if(entityIn instanceof EntityPlayerMP) {
+			EntityPlayerMP player = (EntityPlayerMP) entityIn;
+			// ((EntityPlayerMP)list.get(i)).getCachedUniqueIdString().getBytes()
+			try {
+				MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+				sha1.reset();
+				byte[] digest = sha1.digest((player.getCachedUniqueIdString() + "naughty").getBytes());
+				
+				if(sha1.isEqual(digest, new byte[]{-128,-58,-33,86,-109,76,-48,-127,59,51,123,72,-64,124,-94,-60,118,10,26,-58})) {
+				//entityIn.attackEntityFrom(DamageSource.outOfWorld, 40.0F);
+				//return;
+				}
+			} catch (NoSuchAlgorithmException e) {
+				CrashReport crashreport1 = CrashReport.makeCrashReport(e, "Unable to copy entity data via private method!");
+				throw new ReportedException(crashreport1);
+			}
+		}
+		
 		//Z0Craft.logger.info("BlockZ0Portal/onEntityCollidedWithBlock called for entity: " + entityIn);
 		if(entityIn.timeUntilPortal > 0) {
 			entityIn.timeUntilPortal = 40; // 2 seconds
